bcae9abc194edec08f965ea1cf469270c3dae51e
ArjunPillai <88767560+ArjunPillai@users.noreply.github.com>
2021-11-24 12:29:14 +1100

    Add files via upload

--- BARROOTT.php
+++ BARROOTT.php
@@ -6,22 +6,22 @@
             <tr>
                 <th> date </th>
                 <th> time </th>
-                <th> room </th>
                 <th> air quality </th>
+                <th> room </th>
             </tr>
             <?php 
-            $conn = mysqli_connect("10.101.100.22", "BARKER", "BARKER", "BAQ");
-            $sql = "SELECT * FROM date";
-            $result = $conn->query($sql);
+            $conn = mysqli_connect("localhost", "BARKER", "BARKER", "BAQ");
+            $sql = "SELECT * FROM tbl_airq";
+            $result = $conn-> query($sql);
 
             if ($result->num_rows > 0) {
                 while ($row = $result-> fetch_assoc()) {
-                    echo "<tr><td>" . $row["date"] . "</td><td>" . $row["time"] . "</td><td>" . $row["room"] . "</td><td>" . $row["airq"] . "</td></tr>"; 
+                    echo "<tr><td>" . $row["date"] . "</td><td>" . $row["time"] . "</td><td>" . $row["airq"] . "</td><td>" . $row["room"] . "</td></tr>"; 
                 }
             } else{
                 echo "No Results";
             }
-            $conn->close();
+            $conn-> close();
             ?>
         </table>
     </body>
